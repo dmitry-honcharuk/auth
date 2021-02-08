6b6771a278560e999500b6af3e842c9d0c1130ce
dmitry.honcharuk <dmitry.honcharuk@gmail.com>
2021-02-09 01:06:19 +0200

    test page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import Link from 'next/link';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ul>
+        <li>
+          <Link href='/'>Home</Link>
+        </li>
+        <li>
+          <Link href='/test'>Test page</Link>
+        </li>
+      </ul>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
