cd79803e7d06580b7e23218cf39875aae6077c3c
Dmitry Honcharuk <dmitry.honcharuk@gmail.com>
2022-01-29 02:23:46 +0200

    added local mongo connection added prettier

--- src/app/implementations/repositories/mongo/connectionUrl.ts
+++ src/app/implementations/repositories/mongo/connectionUrl.ts
@@ -4,4 +4,8 @@ import {
   MONGO_DB_USER,
 } from '../../../config/env';
 
-export const CONNECTION_URL = `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@main.mp8ja.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority`;
+const isDev = process.env.NODE_ENV === 'development';
+
+export const CONNECTION_URL = isDev
+  ? 'mongodb://localhost:27017?retryWrites=true&w=majority'
+  : `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@main.mp8ja.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority`;
